283b0d669605b904a038c4e2db2ddde241e9832d
Selam Alem <selamtalem@gmail.com>
2021-10-21 14:11:32 -0500

    Resubmitting work

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -15,43 +15,35 @@ console.log('Test - should say "Hello World!"', hello());
 // 2. Function to return an personalized hello, using the `name` argument.
 //    for example 'Hello, Jo!', or 'Hello, Stacy!'
 function helloName( name ) {
-  return 'Hello, Selam!'
+  return `Hello, ${name}!`
 }
 // Remember to call the function to test
-console.log( 'Test - personalized hello:', helloName());
+console.log( 'Test - personalized hello:', helloName('Matt'));
+console.log( 'Test - personalized hello:', helloName('Abbysinia'));
+console.log( 'Test - personalized hello:', helloName('Euphratius'));
 
 // 3. Function to add two numbers together & return the result
-function addNumbers( firstNumber ) {
+function addNumbers( firstNumber, secondNumber ) {
   // return firstNumber + secondNumber;
-  return 3 + 2;
+  return firstNumber + secondNumber;
 }
-console.log( 'Adding two numbers 3 & 2:', addNumbers());
+console.log( 'Adding two numbers 3 & 2:', addNumbers(3,2));
+console.log( 'Adding two numbers 4 & 35:', addNumbers(4,35));
+console.log( 'Adding two numbers 39 & 304:', addNumbers(39,304));
+console.log( 'Adding two numbers 12 & 81:', addNumbers(12,81));
 
 // 4. Function to multiply three numbers & return the result
-function multiplyThree(){
-  return 2 * 3 *2;
+function multiplyThree( firstNumber, secondNumber, thirdNumber){
+  return firstNumber * secondNumber * thirdNumber;
 }
-console.log( 'Multiplying 3 Numbers (2x3x2)', multiplyThree());
+console.log( 'Multiplying 3 Numbers (2x3x2)', multiplyThree(2,3,2));
+console.log( 'Multiplying 3 Numbers (8x2x10)', multiplyThree(8,2,10));
+console.log( 'Multiplying 3 Numbers (5x3x6)', multiplyThree(5,3,6));
 
 // 5. Function that will return true if a number is positive,
 //    or greater than zero, and false otherwise
 function isPositive( number ) {
-  if ( 3 > 0 ){
-    return    ;
-  }
-
-    return false;
-}
-
-function isPositive( number ) {
-  if ( 0 > 0 ){
-    return true;
-  }
-    return false;
-}
-
-function isPositive( number ) {
-  if ( -3 > 0 ){
+  if ( +number > 0 ){
     return true;
   }
     return false;
@@ -59,9 +51,10 @@ function isPositive( number ) {
 
 // Call the function to test each outcome (true & false)
 // Write a separate console.log statement for each outcome
-console.log( 'isPositive - should say true', isPositive(3) );
-console.log( 'isPositive - should say false', isPositive(0) );
-console.log( 'isPositive - should say false', isPositive(-3) );
+
+console.log( 'Testing if the number 3 is positive & greater than 0:', isPositive(3));
+console.log( 'Testing if the number 0 is positive & greater than 0:', isPositive(0));
+console.log( 'Testing if the number -3 is positive & greater than 0:', isPositive(-3));
 
 
 // 6. Function to return the _last_ item in an array. If the
@@ -80,17 +73,20 @@ console.log(getLast([ 'red', 'blue', 'orange', 'yellow']));
 // 7. Function to find a value in an array. Return true if the
 //    value is found and false otherwise. Use a loop;
 //    DO NOT use Array.includes, Array.indexOf, or Array.find
+
 function find(value, array) {
-   for(let i = 0; i < arrray.length; i++ ) {
-      if(i === array[i]) {
+   for(let i = 0; i < array.length; i++ ) {
+      if(value === array[i]) {
          return true;
       }
    }
    return false;
 }
 
-let favNumbers = ["2","23","7","17","9"];
-console.log(find("7", favNumbers));
+let favNumbers = ["2","23","7","17","9", "32", "8"];
+console.log('Finding the number 7 in the array:', find("7", favNumbers));
+console.log('Finding the number 20 in the array:', find("20", favNumbers));
+//console.log(find("7", favNumbers));
 
 // ----------------------
 // Stretch Goals - I feel like i need to personally do more practice on these !ß
